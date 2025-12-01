63477cca06b25553449c46ed61cdcad5f04ce1e0
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 19:32:58 -0300

    git

---
+++ JavaScript/002/operadores-comparacao-2.js
@@ -0,0 +1,16 @@
+/* Operadores de comparação
+
+    >       -> maior
+    >=      -> maior ou igual
+    <       -> menor
+    <=      -> menor ou igual
+
+*/
+
+const firstNumber = 10
+const seccondNumber = 20
+
+console.log(firstNumber > seccondNumber) // false
+console.log(firstNumber < seccondNumber) // true
+console.log(firstNumber >= seccondNumber) // false
+console.log(firstNumber <= seccondNumber) // true
---
+++ JavaScript/002/operadores-comparacao.js
@@ -0,0 +1,17 @@
+/*
+
+== -> Igual // Ele compara o valor mmas não o tipo do valor
+=== -> TOTALMENTE IGUAL // Ele verifica o tipo de dado
+
+!= -> Diferente // Ele compara o valor, mas não compara o tipo do valor
+!== -> TOTALMENTE DIFERENTE // Ele verifica o tipo do dado!
+ */
+
+const firtPerson = "João"
+const seccondPerson = "Maria"
+
+const firstNumber = "23"
+const seccondNumber = 23
+
+console.log(firtPerson !== seccondPerson)
+console.log(firstNumber === seccondNumber)
