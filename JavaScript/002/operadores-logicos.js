25d1db32ab11cb65e8c2f0bad37b9424e3453f34
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 19:35:05 -0300

    Operadores lógicos

---
+++ JavaScript/002/operadores-logicos.js
@@ -0,0 +1,17 @@
+/* OPERADORES LÓGICOS
+
+&& -> E
+    true && true = true
+    true && false = false
+    false && false = false
+
+||  -> Ou
+    true || true = true
+    true || false = true
+    false || false = false
+
+!   -> Negação
+    !true = false
+    !false = true
+*/
+
