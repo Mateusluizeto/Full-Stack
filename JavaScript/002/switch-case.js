6a108bdbe21217b454000c7f9932fbed3c716b71
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 20:56:22 -0300

    Switch-case

---
+++ JavaScript/002/switch-case.js
@@ -0,0 +1,23 @@
+/* CONTROLE DE FLUXO - CONDICIONAIS - SWITCH CASE
+
+*/
+
+const temperature = 36;
+
+switch(temperature){
+    case 30:
+        console.log("A pessoa está com a temperatura MUITO abaixo do normal")
+        break;
+
+        case 33:
+            console.log("A pessoa está com a temperatura abaixo do normal")
+        break
+
+        case 36:
+            console.log('A pessoa está com a temperatura abaixo do normal')
+        break
+        
+    default:
+        console.log('temperatura não encontrada')
+        break;
+}
