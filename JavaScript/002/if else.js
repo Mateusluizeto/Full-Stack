d56f3ae6b1afa18c75dd4419e74d25034ebb2d34
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 20:31:38 -0300

    If e Else

---
+++ JavaScript/002/if else.js
@@ -0,0 +1,14 @@
+/* CONTROLE DE FLUXO - CONDICIONAIS - IF/ELSE
+    if = SE
+    else = se não
+*/
+
+const age = 18
+
+if(age >= 18){
+    console.log('A pessoa é maior de idade')
+} else{
+    console.log('A pessoa não é maior de idade')
+}
+
+console.log('TERMINOU')
