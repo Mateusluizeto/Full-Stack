fbefb59a968ae3d49858bac904e153a26f93ed8d
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 18:32:40 -0300

    Propriedades Math em js

---
+++ JavaScript/002/math.js
@@ -0,0 +1,11 @@
+/*
+Math
+*/
+
+console.log(Math.PI) // Imprime o número PI
+console.log(Math.pow(2,2)) // Para elevar um número, ex: 2 elevado a 2
+console.log(Math.sqrt(25)) // Raiz quadrada
+console.log(Math.random()) // Mostra um número aleatório entre 0 e 1
+console.log(Math.ceil(23.6)) // Arredondar um número para cima
+console.log(Math.floor(23.6)) // Arredondar um número para baixo
+console.log(Math.round(23.6)) // Arredonda para o número mais próximo 
