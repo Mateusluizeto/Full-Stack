e2afc9e53bc893b983acc88cf5749750dc900a56
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-01 17:51:23 -0300

    Começando com JavaScript

---
+++ JavaScript/001/variables.js
@@ -0,0 +1,25 @@
+let number1 = 1
+let number2 = 2
+const number3 = 3
+
+// Variáveis
+
+/*
+Declaração:
+
+    var: escopo de função, pode ser redeclarada (legado, menos usado hoje)
+    let: escopo de bloco, pode ser reatribuída mas não redeclarada
+    const: escopo de bloco, valor não pode ser reatribuído (use para valores constantes)
+
+Nomenclatura:
+
+    Nomes descritivos e em camelCase (ex: minhaVariavel)
+    Começa com letra, _ ou $
+    Case-sensitive: nome é diferente de Nome
+
+Tipos de dados:
+
+    Primitivos: string, number, boolean, null, undefined, symbol, bigint
+    Objetos: object, array, function
+
+*/
