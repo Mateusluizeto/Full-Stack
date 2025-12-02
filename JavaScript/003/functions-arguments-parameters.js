e524a57326bbce53e21c8211ea068def275b1d43
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-02 15:31:12 -0300

    Parâmetros e argumentos em functions

---
+++ JavaScript/003/functions-arguments-parameters.js
@@ -0,0 +1,15 @@
+/*
+    functions
+
+    Arguments and parameters
+*/
+
+
+
+function sum(number1, number2){ // Parâmetros
+    console.log(number1 + number2)
+}
+
+sum(2,3) // Argumentos
+sum(5,5)
+sum(30,20)
