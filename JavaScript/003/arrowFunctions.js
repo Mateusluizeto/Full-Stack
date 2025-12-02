e0c646ecf62097913e687ab0e7aacf393fffbdff
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2025-12-02 19:01:09 -0300

    Arrow functions

---
+++ JavaScript/003/arrowFunctions.js
@@ -0,0 +1,15 @@
+/*
+    - Arrow Functions
+*/  
+
+const sum = (number1, number2) => number1 + number2
+
+const firstNumber = 10
+const seccondNumber = 20
+
+console.log(`O primeiro número é ${firstNumber}`)
+console.log(`O segundo número é ${seccondNumber}`)
+
+const result = sum(firstNumber, seccondNumber)
+
+console.log(`A soma dos dois número é ${result}`)
