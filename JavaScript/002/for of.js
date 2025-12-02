2b035ac8fc24377d9ee45cb15cad7c171ecf02a2
MateusLuizetoReis <mluizetoreis@gmail.com>
2025-12-02 12:46:29 -0300

    Estrutura de repetição - for of

---
+++ JavaScript/002/for of.js
@@ -0,0 +1,14 @@
+/*
+    Estrutura de repetição - for of
+*/
+
+let myName = 'Rodolfo'
+let allNames = ['Maria', 'João', 'Pedro', 'Gilberto']
+
+for(let name of myName){
+    console.log(name) // Resultado = R o d o l f o
+}
+
+for(let names of allNames){
+    console.log(names)
+}
