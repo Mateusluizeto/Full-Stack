6100e2f2124214855d113bb77d52227a28e1d655
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2025-12-03 15:16:28 -0300

    Métodos - ARRAY

---
+++ JavaScript/004/arrays.js
@@ -0,0 +1,21 @@
+/*
+    ARRAY - Métodos
+*/
+
+const students = ['João', 'Maria', 'Pedro', 'Carla', 'Bia', 'Maurício']
+
+students.push('Mateus') // Adicionar mais um item no array
+students.push('Marcelo')
+students.sort()
+ 
+console.log(students)
+
+// push
+// length
+// sort
+// delete
+
+// splice (unir / ligar)
+// slice (fatiar)
+// pop
+// shift
