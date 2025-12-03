3969292df25e9e5b93c2768c6b7db6c193c1ccef
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2025-12-03 15:57:53 -0300

    Métodos - ARRAYS

---
+++ JavaScript/004/arrays-2.js
@@ -0,0 +1,20 @@
+/*
+    ARRAY - Métodos
+*/
+
+const students = ['João', 'Maria', 'Pedro', 'Carla', 'Bia', 'Maurício']
+
+
+// push
+// length
+// sort
+// delete
+
+// splice (unir / ligar)
+// slice (fatiar)
+// pop
+// shift
+
+students.pop()
+
+console.log(students)
