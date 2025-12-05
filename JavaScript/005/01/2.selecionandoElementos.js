dd863cdd9ad304f15b012085d01b4984c66718ca
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2025-12-05 19:18:10 -0300

    getElements

---
+++ JavaScript/005/01/2.selecionandoElementos.js
@@ -0,0 +1,21 @@
+/*const element = document.getElementById('main-text')
+console.log(element)*/
+
+/*const element = document.getElementsByClassName('paragraph')
+console.log(element.item(0).textContent)*/
+
+/*const element = document.getElementsByTagName('p')
+console.log(element)*/
+
+/*const element = document.querySelector('.paragraph')
+console.log(element)*/
+
+/*const element = document.querySelectorAll('p')
+
+element.forEach((elementSeparate) => {
+    console.log(elementSeparate)
+}) // é possível usar o forEach, Map e o Filter quando o elemento esta em um querySelectorAll
+*/
+
+const element = document.getElementsByName('my-input')
+console.log(element)
