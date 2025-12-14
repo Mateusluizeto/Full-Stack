b773c5dd4d4629db3251b5d32e87f4efbaf51611
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2025-12-14 11:06:59 -0300

    Entendendo o import e export

---
+++ JavaScript/006 (Iniciando com o node)/06/modules/functions.js
@@ -0,0 +1,11 @@
+function sum(firstNumber, seccondNumber) {
+    return firstNumber + seccondNumber
+}
+
+function multi(firstNumber, seccondNumber){
+    return firstNumber * seccondNumber
+}
+
+
+export {sum, multi} /* Exporta a função, se torna possível acessar
+                            a função de qualquer outro arquivo que estiver nesta pasta*/
---
+++ JavaScript/006 (Iniciando com o node)/06/modules/index.js
@@ -0,0 +1,5 @@
+//const functions = require('./functions')
+import { sum, multi } from "./functions.js"
+
+console.log(sum(2, 2)) // Soma 2+2
+console.log(multi(5, 5)) // Multiplica 5x5
